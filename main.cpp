00deadfa11247fe75f19f920c8d5a8428e1edd35
valeriaconte <128838224+valeriaconte@users.noreply.github.com>
2023-04-14 18:29:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float a=0, b=0, x=0, y=0, z=0;
+    cout<< "inserire quattro numeri reali\n";
+    cin>> a >> b >> x >> y;
+
+    if (x<0 and y>0) {
+        z = a*x-b*y;
+    }
+    else if (x>=0 and y<=0) {
+        z = a*x*x-b*y;
+    }
+    else {
+        z = a*x+b*y*y;
+    }
+    cout<< z;
+    return 0;
 }
